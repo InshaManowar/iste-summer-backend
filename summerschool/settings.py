0016b38fd4b41174ab31f01a4a7adf0de04899ac
inshamanowar <inshamanowar22@gmail.com>
2021-05-29 23:18:32 +0530

    authviews

--- summerschool/settings.py
+++ summerschool/settings.py
@@ -106,6 +106,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
+EMAIL_USE_TLS = True 
+EMAIL_USE_SSL = False 
+EMAIL_HOST = 'smtp.gmail.com' 
+EMAIL_PORT = 587  
+EMAIL_HOST_USER = 'isterecruitments2020@gmail.com' 
+EMAIL_HOST_PASSWORD = 'ohrdkoowygjivjse' 
 
 
 # Internationalization
--- summerschool/urls.py
+++ summerschool/urls.py
@@ -2,25 +2,22 @@ from django.conf.urls import url
 from django.urls import path, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('api/course/', include('course.urls')),
-    
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'), 
-        name='password_change_done'),
-    
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='accounts/password_change.html'), 
-        name='password_change'),
-
     path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_done.html'),
         name='password_reset_done'),
-
     path('password_reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
-    
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset_form.html'), name='password_reset'),
-    
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+    path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
      name='password_reset_complete')   
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    
