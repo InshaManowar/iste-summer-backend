0c0e366868a6297d0e9d3d281b68bcf433f8c894
oam-mit <omkar.masur@gmail.com>
2021-07-23 01:01:21 +0530

    added 404 and profile url

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('tasks/<slug:category>', views.home, name="categories"),
     path('tasks/<slug:category>/<str:task_uuid>', views.home, name="tasks"),
+    path('profile', views.home, name="home"),
 ]
